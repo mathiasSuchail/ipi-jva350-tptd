7bd5231c88082dbc764a1dcf957df9b6a05c807d
Mathias SUCHAIL <mathias.suchail@yahoo.com>
2024-03-12 08:57:14 +0100

    Test+implementation estDansLaPlage()

--- src/test/java/com/ipi/jva350/model/EntrepriseTest.java
+++ src/test/java/com/ipi/jva350/model/EntrepriseTest.java
@@ -4,11 +4,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public class EntrepriseTest {
     @Test
-    public void testJoursFeries() {
+    public void testEstDansPlageTrue() {
+        // Given :
+        LocalDate date = LocalDate.of(2023, 6, 28);
+        // When :
+        boolean res = Entreprise.estDansPlage(date, LocalDate.of(2023, 6, 1), LocalDate.of(2023, 6, 30));
+        // Then :
+        Assertions.assertTrue(res);
+    }
+
+    @Test
+    public void testEstDansPlageFalse() {
+        // Given :
+        LocalDate date = LocalDate.of(2023, 12, 28);
+        // When :
+        boolean res = Entreprise.estDansPlage(date, LocalDate.of(2023, 6, 1), LocalDate.of(2023, 6, 30));
+        // Then :
+        Assertions.assertFalse(res);
+    }
+
+    @Test
+    public void testJoursFeriesTrue() {
         // test feriés :
         Assertions.assertTrue(Entreprise.estJourFerie(LocalDate.of(2024, 1, 1)));
         Assertions.assertTrue(Entreprise.estJourFerie(LocalDate.of(2024, 4, 1)));
@@ -20,10 +39,14 @@ public class EntrepriseTest {
         Assertions.assertTrue(Entreprise.estJourFerie(LocalDate.of(2024, 11, 1)));
         Assertions.assertTrue(Entreprise.estJourFerie(LocalDate.of(2024, 11, 11)));
         Assertions.assertTrue(Entreprise.estJourFerie(LocalDate.of(2024, 12, 25)));
+    }
 
-
+    @Test
+    public void testJoursFeriesFalse() {
         // test non fériés :
         Assertions.assertFalse(Entreprise.estJourFerie(LocalDate.of(2024, 1, 2)));
     }
+
+
 
 }
